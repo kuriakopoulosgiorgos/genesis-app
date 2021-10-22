43dd853c1e72d286d7a34738a0bc2d3d4e1e58bc
George Kyriakopoulos <kuriakopoulosgiorgos@gmail.com>
2021-10-22 23:20:34 +0300

    View details page

--- src/main/angular/src/app/products/products-routing.module.ts
+++ src/main/angular/src/app/products/products-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductsGridContainerComponent } from './products-grid/products-grid.container';
+import { ProductDetailsContainerComponent } from './product-details/product-details.container';
 
 const routes: Routes = [
   { path: '', component: ProductsGridContainerComponent },
+  { path: 'details/:id', component: ProductDetailsContainerComponent },
 ];
 
 @NgModule({
--- src/main/angular/src/app/products/products.module.ts
+++ src/main/angular/src/app/products/products.module.ts
@@ -5,23 +5,30 @@ import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsGridContainerComponent } from './products-grid/products-grid.container';
 import { ProductsGridComponent } from './products-grid/products-grid.component';
 import { SharedModule } from '../shared/shared.module';
-import { NgbPagination, NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCarouselModule, NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { ProductCardComponent } from './product-card/product-card.component';
+import { ProductDetailsContainerComponent } from './product-details/product-details.container';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { RenderModule } from '../render/render.module';
 
 
 @NgModule({
   declarations: [
      ProductsGridContainerComponent,
      ProductsGridComponent,
-     ProductCardComponent
+     ProductCardComponent,
+     ProductDetailsContainerComponent,
+     ProductDetailsComponent
     ],
   imports: [
     SharedModule,
     CommonModule,
     ProductsRoutingModule,
     FormsModule,
-    NgbPaginationModule
+    NgbPaginationModule,
+    NgbCarouselModule,
+    RenderModule
   ]
 })
 export class ProductsModule { }
