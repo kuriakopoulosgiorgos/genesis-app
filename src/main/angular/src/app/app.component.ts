2834d611ab09e2249fa600ff5dc0af48354dd48a
George Kyriakopoulos <kuriakopoulosgiorgos@gmail.com>
2021-10-24 21:00:02 +0300

    change language

--- src/main/angular/src/app/app.component.ts
+++ src/main/angular/src/app/app.component.ts
@@ -9,7 +9,30 @@ import { TranslateService } from '@ngx-translate/core';
 export class AppComponent {
   title = 'genesis';
 
+  readonly languages = [ 'en', 'el' ];
+
+  readonly languageIcons = {
+    "en": "gb",
+    "el": "gr"
+  }
+
+  currentLanguage: string;
+
   constructor(private translateService: TranslateService) {
-    translateService.setDefaultLang('en');
+    let language = localStorage.getItem("language");
+    // if not previously set
+    if(!language) {
+    // find the language from the browser settings if it is supported
+      language = this.languages.find((l) => l === navigator.language);
+    }
+    this.currentLanguage = language ? language : 'en';
+    localStorage.setItem("language", language);
+    this.translateService.setDefaultLang(this.currentLanguage);
+  }
+
+  changeLanguage(language: string): void {
+    this.currentLanguage = language;
+    localStorage.setItem("language", this.currentLanguage);
+    this.translateService.setDefaultLang(language);
   }
 }
